1ba00093dd1d7683e5d42a60b547eff68bb8aa52
onsjannet <onsjannet@gmail.com>
2020-09-17 19:01:14 +0100

    added nop

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * nop - a function that doesn’t do anything
+ * @head: pointer to a pointer to a struct of type stack_t
+ * @line_number: line number of instruction
+ *
+ * Return: void
+ */
+
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
