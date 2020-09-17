87837a3d21c891262ef2ffefce17d0a6ff242990
onsjannet <onsjannet@gmail.com>
2020-09-17 14:14:37 +0100

    added push and pall file

---
+++ push_pall.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+
+/**
+ * push - pushes new node to the end of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number
+ *
+ * Return: void
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+	char *number;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}	
+
+	number = strtok(NULL, DELIMS);
+	if (number == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = atoi(number);
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
+
+/**
+ * _pall - prints all the values on the stack starting from the top
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void) line_number;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
